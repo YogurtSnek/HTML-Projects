b6b3ecf783df9ec274267b9fd2e05e0c04c35403
Yogurt Snek <75657131+YogurtSnek@users.noreply.github.com>
2023-04-24 14:49:05 +0000

    Homework done for first JS

---
+++ HTML Projects/Tareas/Practica inicios de JS/JS/basic.js
@@ -0,0 +1,9 @@
+window.alert("You are entering my website, I hope it is to your liking");
+document.getElementById("Des").innerHTML = "The Solar System is the gravitationally bound system of the Sun and the objects that orbit it.";
+document.writeln("<ul>"
++"\n<li>The Solar System has Eight Planets</li>"
++"\n<li>The Solar System has been around for billions of years</li>"
++"\n<li>The Sun is 93 million miles away from us</li>"
++"\n</ul>");
+
+console.log("And did you know... Venus can be seen from earth");
