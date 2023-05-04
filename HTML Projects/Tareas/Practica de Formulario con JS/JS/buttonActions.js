fddcbcbb739ec443ba1b77088c27eba1236356ed
Yogurt Snek <75657131+YogurtSnek@users.noreply.github.com>
2023-05-04 20:13:11 +0000

    WAWAWAWAW

--- HTML Projects/Tareas/Practica de Formulario con JS/JS/buttonActions.js
+++ HTML Projects/Tareas/Practica de Formulario con JS/JS/buttonActions.js
@@ -8,19 +8,7 @@ for (let i = 1; i < questions.length; i++) {
     +"animation-timing-function: cubic-bezier(.32,.88,.49,1.39);"
     +"animation-iteration-count: 1;"
     +"animation-fill-mode: forwards;"
-    +"left: 150%");
-
-}
-
-function questionMoves(direction) {
-    //left is 0, right is 1
-    if (direction == 1) {
-        questions[currentQ].setAttribute("style", "left: 50%")
-        questions[currentQ-1].setAttribute("style", "left: -25%")
-    } else {
-        questions[currentQ].setAttribute("style", "left: 50%")
-        questions[currentQ+1].setAttribute("style", "left: 125%")
-    }
+    +"left: 200%");
 
 }
 
@@ -30,25 +18,23 @@ function OnClickPrev() {
         
         console.log("Current Questions: " + currentQ)
 
-        questions[currentQ].setAttribute("style", "animation-name: slideRight;"
+        questions[currentQ].setAttribute("style", "animation-name: slideCenterRight;"
         +"animation-duration: 2s;"
         +"animation-direction: normal;"
-        +"animation-timing-function: cubic-bezier(.32,.88,.49,1.39);"
+        +"animation-timing-function: cubic-bezier(.51,1.56,.63,1.09);"
         +"animation-iteration-count: 1;"
         +"animation-fill-mode: forwards;");
 
         currentQ -= 1
         
-        questions[currentQ].setAttribute("style", "animation-name: slideRight;"
+        questions[currentQ].setAttribute("style", "animation-name: slideLeftCenter;"
         +"animation-duration: 2s;"
         +"animation-direction: normal;"
-        +"animation-timing-function: cubic-bezier(.32,.88,.49,1.39);"
+        +"animation-timing-function: cubic-bezier(.51,1.56,.63,1.09);"
         +"animation-iteration-count: 1;"
         +"animation-fill-mode: forwards;");
         
-        setTimeout(() => {
-            questionMoves(0)
-        }, 2000);
+        
 
     }
 }
@@ -58,25 +44,21 @@ function OnClickNext() {
 
         console.log("Current Questions: " + currentQ)
 
-        questions[currentQ].setAttribute("style", "animation-name: slideLeft;"
+        questions[currentQ].setAttribute("style", "animation-name: slideCenterLeft;"
         +"animation-duration: 2s;"
         +"animation-direction: normal;"
-        +"animation-timing-function: cubic-bezier(.32,.88,.49,1.39);"
+        +"animation-timing-function: cubic-bezier(.51,1.56,.63,1.09);"
         +"animation-iteration-count: 1;"
         +"animation-fill-mode: forwards;");
 
         currentQ += 1
         
-        questions[currentQ].setAttribute("style", "animation-name: slideLeft;"
+        questions[currentQ].setAttribute("style", "animation-name: slideRightCenter;"
         +"animation-duration: 2s;"
         +"animation-direction: normal;"
-        +"animation-timing-function: cubic-bezier(.32,.88,.49,1.39);"
+        +"animation-timing-function: cubic-bezier(.51,1.56,.63,1.09);"
         +"animation-iteration-count: 1;"
         +"animation-fill-mode: forwards;");
-
-        setTimeout(() => {
-            questionMoves(1)
-          }, 2000);
 
     } else {
         console.log("End of Questions.")
