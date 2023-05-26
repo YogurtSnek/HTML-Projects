82adb9034794c40fe599ea42cd3178eb78de962c
YogurtSnek <pootissonic1808@gmail.com>
2023-05-25 22:52:08 -0700

    AJAX Calculator Complete

--- HTML Projects/Tareas/Practica de Calculadora con AJAX/JS/buttonActions.js
+++ HTML Projects/Tareas/Practica de Calculadora con AJAX/JS/buttonActions.js
@@ -9,40 +9,17 @@ function Summation() {
         for (let i = sValue; i < limit; i++) {
 
             Summation += (i*i)
-            Procedure += "("+ i + " * " + i + ") + "
+            Procedure += "("+ i + " * " + i + ")\n + "
         }
 
         Summation += (limit*limit)
         Procedure += "("+ limit + " * " + limit + ")"
 
-        console.log(Procedure)
-
         document.getElementById("Summation").value = Summation
 
-        var xmlhttp;
-        if (window.XMLHttpRequest) {
-            // code for IE7+, Firefox, Chrome, Opera, Safari
-            xmlhttp = new XMLHttpRequest();
-        }
-        else {
-            // code for IE6, IE5
-            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-        }
-        
-        xmlhttp.onreadystatechange=function()
-        {
-        if (xmlhttp.readyState==4 && xmlhttp.status==200)
-        {
-            console.log("Successful Connection.")
-        }
-        }
-      
-        xmlhttp.open("POST","PHP/FileWrite.php",true);
-        xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-        xmlhttp.send("Procedure="+ encodeURIComponent(Procedure));
+        saveProcedure(Procedure)
 
-
-        writeProcedure("Summation-Procedure");
+        writeProcedure("Summation-Procedure")
       
     } else {
         alert("Starting Value of the Summation cannot be over the limit! \nCheck console for details.")
@@ -52,12 +29,82 @@ function Summation() {
 }
 
 function Fibonacci() {
+    var Procedure = ""
+    var n1 = 0 
+    var temp = 0
+    var n2 = 1
 
+    for (var i = 0;i < 18; i++) {
+        temp = n1 + n2
+        Procedure += n1 + " + " + n2 + " = " + temp + "\n"
+        n1 = n2
+        n2 = temp
+    }
+
+    document.getElementById("Fibonacci-Sequence").value = temp
+
+    saveProcedure(Procedure)
+
+    writeProcedure("Fibonacci-Procedure")
 }
 
 function Factorial() {
+    var num = document.getElementById("Factorial-ToDisplay").value
+
+    if (num > 0) {
+        var factorial = 1;
+        var Procedure = ""
+
+        for (let i = num; i > 1; i--) {
+            factorial *= i;
+            Procedure += i + " * "
+            if (i%5 == 0) {
+                Procedure += "\n"
+            }
+        }
+
+        factorial *= 1;
+        Procedure += 1 + " = " + factorial
+
+        document.getElementById("Factorial").value = factorial   
+
+        saveProcedure(Procedure)
+
+        writeProcedure("Factorial-Procedure")
+
+
+    } else if (num == 0) {
+        document.getElementById("Factorial").value = "1"
+        document.getElementById("Factorial-Procedure").value = "0! = 1"
+    } else {
+        alert("The number cannot be negative nor can it have symbols.")
+    }
 
 }
+
+function saveProcedure(Procedure) {
+    var xmlhttp;
+    if (window.XMLHttpRequest) {
+        // code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp = new XMLHttpRequest();
+    }
+    else {
+        // code for IE6, IE5
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    
+    xmlhttp.onreadystatechange=function() {
+        if (xmlhttp.readyState==4 && xmlhttp.status==200)
+        {
+            console.log("Successful Connection.")
+        }
+    }
+    
+    xmlhttp.open("POST","PHP/FileWrite.php",true);
+    xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    xmlhttp.send("Procedure="+ encodeURIComponent(Procedure));
+}
+
 
 function writeProcedure(ID) {
     var xmlhttp = new XMLHttpRequest();
@@ -65,4 +112,12 @@ function writeProcedure(ID) {
     xmlhttp.send();
     console.log(xmlhttp.response);
     document.getElementById(ID).value = xmlhttp.response;
-  }
+}
+
+function factorialCalc(num) {
+    if(num === 0 || num === 1) {
+        return 1;
+      } else {
+        return num * factorialCalc(num - 1);
+      }
+}
