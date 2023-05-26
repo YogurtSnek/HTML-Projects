84f428dafaa049c524eebbb9d8b75a0bc09bbe27
YogurtSnek <pootissonic1808@gmail.com>
2023-05-25 19:18:06 -0700

    Summation Complete on AJAX Calculator

--- HTML Projects/Tareas/Practica de Calculadora con AJAX/JS/buttonActions.js
+++ HTML Projects/Tareas/Practica de Calculadora con AJAX/JS/buttonActions.js
@@ -1,23 +1,53 @@
-const fs = require('fs');
-
-
-
-
 function Summation() {
-    let limit = Number(document.getElementById("Summation-Limit").innerHTML)
-    let sValue = Number(document.getElementById("Summation-startingValue").innerHTML)
+    var limit = Number(document.getElementById("Summation-Limit").value)
+    var sValue = Number(document.getElementById("Summation-startingValue").value)
+    
 
     if (sValue < limit) {
+        var Procedure = ""
+        var Summation = 0
         for (let i = sValue; i < limit; i++) {
 
+            Summation += (i*i)
+            Procedure += "("+ i + " * " + i + ") + "
         }
-    
-        fs.writeFile('output.txt', 'Hello World.', err => {
-            if (err) console.error(err);
-            else console.log('Data written to file successfully.');
-        });
-    }
 
+        Summation += (limit*limit)
+        Procedure += "("+ limit + " * " + limit + ")"
+
+        console.log(Procedure)
+
+        document.getElementById("Summation").value = Summation
+
+        var xmlhttp;
+        if (window.XMLHttpRequest) {
+            // code for IE7+, Firefox, Chrome, Opera, Safari
+            xmlhttp = new XMLHttpRequest();
+        }
+        else {
+            // code for IE6, IE5
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        
+        xmlhttp.onreadystatechange=function()
+        {
+        if (xmlhttp.readyState==4 && xmlhttp.status==200)
+        {
+            console.log("Successful Connection.")
+        }
+        }
+      
+        xmlhttp.open("POST","PHP/FileWrite.php",true);
+        xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+        xmlhttp.send("Procedure="+ encodeURIComponent(Procedure));
+
+
+        writeProcedure("Summation-Procedure");
+      
+    } else {
+        alert("Starting Value of the Summation cannot be over the limit! \nCheck console for details.")
+        console.log("Limit: " + limit + "\nStarting Value: " + sValue)
+    }
     
 }
 
@@ -28,3 +58,11 @@ function Fibonacci() {
 function Factorial() {
 
 }
+
+function writeProcedure(ID) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", "Assets/Procedure.txt",false);
+    xmlhttp.send();
+    console.log(xmlhttp.response);
+    document.getElementById(ID).value = xmlhttp.response;
+  }
--- HTML Projects/Tareas/Practica de Calculadora con AJAX/PHP/FileWrite.php
+++ HTML Projects/Tareas/Practica de Calculadora con AJAX/PHP/FileWrite.php
@@ -2,7 +2,7 @@
 $myFile = "../Assets/Procedure.txt";
 
 $fh = fopen($myFile, 'w') or die("Can't Open File!");
-$stringData = $_POST["name"];
+$stringData = $_POST["Procedure"];
 fwrite($fh, $stringData);
 fclose($fh);
 ?>  
